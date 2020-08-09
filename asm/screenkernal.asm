ZornsLemma/ozmoo

commit 3dc3e495a1b6b9438695a22c7bfae6fa844bf275

Tweak cursor positioning during initialization

asm/screenkernal.asm
@@ -36,6 +36,10 @@ s_init
     lda #$ff
 !ifndef ACORN {
     sta s_current_screenpos_row ; force recalculation first time
+
+    lda #0
+    sta zp_screencolumn
+    sta zp_screenrow
 } else {
     sta s_cursors_inconsistent
 
@@ -53,10 +57,15 @@ s_init
     sty screen_width
     iny
     sty screen_width_plus_1
+
+    ; Pick up the current OS cursor position; this will improve readability if
+    ; any errors occuring during initialization, and it doesn't affect the game
+    ; because we will erase the screen before it starts executing.
+    lda #osbyte_read_cursor_position
+    jsr osbyte
+    stx zp_screencolumn
+    sty zp_screenrow
 }
-    lda #0
-    sta zp_screencolumn
-    sta zp_screenrow
 	; Set to 0: s_ignore_next_linebreak, s_reverse
 !ifndef ACORN {
     ldx #3

templates/loader.bas
@@ -50,7 +50,7 @@ REM and only do the change if they're different.
 IF mode%<>7 THEN MODE 128+mode%
 VDU 23,1,0;0;0;0;
 VDU 19,0,4,0,0,0
-PRINT "Loading, please wait...";
+PRINT "Loading, please wait..."'
 *DIR S
 IF tube% THEN */$.OZMOO2P
 IF shadow% THEN */$.OZMOOSH

commit 685eb9af23b22783cd09ddc3d7cc38c5f1e8e2d9

Fix bug introduced in 3dc3e495a1b6b9438695a22c7bfae6fa844bf275
This was visible on the opening screen of THHGTTG (Z5).

asm/screenkernal.asm
@@ -66,13 +66,14 @@ s_init
 
     ; Pick up the current OS cursor position; this will improve readability if
     ; any errors occuring during initialization, and it doesn't affect the game
-    ; because we will erase the screen before it starts executing.
+    ; because deletable_screen_init_2 calls set_cursor anyway.
     lda #osbyte_read_cursor_position
     sta s_cursors_inconsistent
     jsr osbyte
     stx zp_screencolumn
     sty zp_screenrow
 	; Set to 0: s_ignore_next_linebreak, s_reverse, s_os_reverse
+    lda #0
     ldx #4 ; also s_os_reverse
 -	sta s_ignore_next_linebreak,x
 	dex