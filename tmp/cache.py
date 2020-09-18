240c15740a0ee6ec6a1e7655fb31c796f24b1bb5
Steven Flintham <sgf@lemma.co.uk>
2020-09-18 15:25:56 +0100

    Snapshot

--- tmp/cache.py
+++ tmp/cache.py
@@ -7,13 +7,13 @@ vmap_total_entries = vmap_local_entries + vmap_cache_entries
 # vmap here doesn't simulate the "low" bits containing address. Note that
 # the same timestamp can appear more than once.
 vmap = [x // 2 for x in range(vmap_total_entries)]
-original_vmap = vmap
+original_vmap = vmap[:]
 random.shuffle(vmap)
 
 # cutover is supplied by build script in reality, to avoid doing this
 # sort in Ozmoo code.
 sorted_vmap = sorted(vmap, reverse=True)
-# SFTODO: POSS WANT VMAP_LOCAL_ETNRIES-1, I AM STILL SKETCHING THIS OUT
+# SFTODO: POSS WANT VMAP_LOCAL_ETNRIES-1, I AM STILL SKETCHING THIS OUT - I THINK WHAT I HAVE IS CORRECT, BUT IF WE HAVE NO CACHE ENTRIES WE WOULD INDEX OFF THE END OF THE ARRAY
 cutover = sorted_vmap[vmap_local_entries]
 sorted_vmap = None # stop accidental use
 
@@ -24,7 +24,8 @@ to_index = 0
 while to_index < vmap_local_entries:
     vmap[to_index] = vmap[from_index]
     # load_blocks_from_index(to_index)
-    if vmap[to_index] < cutover and len(cache) < vmap_cache_entries:
+    if vmap[to_index] <= cutover and len(cache) < vmap_cache_entries:
+        assert vmap[to_index] in original_vmap
         cache.append(vmap[to_index])
     else:
         to_index += 1
@@ -32,17 +33,24 @@ while to_index < vmap_local_entries:
 
 last_index = vmap_local_entries - 1
 while from_index < vmap_total_entries:
+    assert vmap[last_index] in original_vmap
     cache.append(vmap[last_index])
-    assert len(cache) < vmap_cache_entries
+    assert len(cache) <= vmap_cache_entries
     vmap[last_index] = vmap[from_index]
     # load_blocks_from_index(last_index)
     from_index += 1
     
-print("cache", cache)
+print("cache", sorted(cache))
 local_vmap = vmap[0:vmap_local_entries]
-print("local vmap", local_vmap)
+print("local vmap", sorted(local_vmap))
 
 
 assert len(cache) <= vmap_cache_entries
+assert len(local_vmap) + len(cache) == len(original_vmap)
 assert sorted(local_vmap + cache) == sorted(original_vmap)
-assert len(set(cache).intersection(set(local_vmap))) == 0
+
+cache_set = set(cache)
+local_vmap_set = set(local_vmap)
+intersection = cache_set.intersection(local_vmap_set)
+if len(intersection) > 0:
+    print "intersection", intersection
