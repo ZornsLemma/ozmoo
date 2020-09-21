a7ae73227a62e9ea6b375bb6622a918a3063a28e
Steven Flintham <sgf@lemma.co.uk>
2020-09-21 02:50:38 +0100

    Snapshot

--- sketch.py
+++ sketch.py
@@ -36,33 +36,47 @@ def template_substitute(template, args):
         
             
 class Executable(object):
-    _filename_templates = {
+    def __init__(self, asm_filename, output_basename, start_address, extra_args):
+        self.output_basename = output_basename
+        pass # SFTODO
+
+
+def make_executable(asm_filename, start_address, extra_args):
+    assert isinstance(start_address, int)
+
+    filename_templates = {
         "ozmoo": "ozmoo${ACORN_ELECTRON:_electron:}${ACORN_SWR:_swr:_tube}${ACORN_NO_SHADOW:_noshadow:}${VMEM::_novmem}${ACORN_SWR_SMALL_DYNMEM:_smalldyn:}_${STARTADDRESS}"
     }
 
-    _cache = {}
+    basename = os.path.splitext(asm_filename)[0]
+    assert basename in filename_templates
+    output_basename = template_substitute(filename_templates[basename], ["-DSTARTADDRESS=%s" % ourhex(start_address)] + extra_args)
+
+    # Not all build parameters have to be reflected in the filename, but we
+    # can't have two builds with different parameters using the same filename.
+    definition = (start_address, set(extra_args))
+    cache_entry = make_executable.cache.get(output_basename, None)
+    if cache_entry is not None:
+        assert cache_entry[0] == definition
+        return cache_entry[1]
+
+    try:
+        e = Executable(asm_filename, output_basename, start_address, extra_args)
+    except: # SFTODO: catch specific exception types
+        e = None
+    make_executable.cache[output_basename] = (definition, e)
+    return e
+make_executable.cache = {}
+
     
-    def __init__(self, asm_filename, start_address, extra_args):
-        assert isinstance(start_address, int)
-        self.basename = os.path.splitext(asm_filename)[0]
-        assert self.basename in self._filename_templates
-        self.output_basename = template_substitute(self._filename_templates[self.basename], ["-DSTARTADDRESS=%s" % ourhex(start_address)] + extra_args)
 
-        # Check that we never build two different executables with the same
-        # filename; this would get confusing.
-        cache_definition = (start_address, set(extra_args))
-        cache_entry = self._cache.setdefault(self.output_basename, cache_definition)
-        assert cache_entry == cache_definition
-            
-        # SFTODO
-
-e = Executable("ozmoo.asm", 0x900, [])
+e = make_executable("ozmoo.asm", 0x900, [])
 print e.output_basename
-e = Executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1"])
+e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1"])
 print e.output_basename
-e = Executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_NO_SHADOW=1", "-DVMEM=1"])
+e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_NO_SHADOW=1", "-DVMEM=1"])
 print e.output_basename
-#e = Executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_ELECTRON=1", "-DACORN_SWR_SMALL_DYNMEM=1", "-DVMEM=1"])
+#e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_ELECTRON=1", "-DACORN_SWR_SMALL_DYNMEM=1", "-DVMEM=1"])
 #print e.output_basename
-e = Executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_ELECTRON=1", "-DACORN_SWR_SMALL_DYNMEM=1", "-DVMEM=1", "-DFOO=1"])
+e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_ELECTRON=1", "-DACORN_SWR_SMALL_DYNMEM=1", "-DVMEM=1", "-DFOO=1"])
 print e.output_basename
