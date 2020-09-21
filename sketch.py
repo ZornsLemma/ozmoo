a12783b739a091a06bf78a6175a01773a08592a7
Steven Flintham <sgf@lemma.co.uk>
2020-09-21 03:08:05 +0100

    Snapshot

--- sketch.py
+++ sketch.py
@@ -35,17 +35,35 @@ def template_substitute(template, args):
     return result
         
             
+# SFTODO: In a few places I am doing set(extra_args) - this is fine if all the elements stand alone like "-DFOO=1", but if there are multi-element entries ("--setpc", "$0900") I will need to do something different. I am not sure if this will be an issue or not.
 class Executable(object):
     def __init__(self, asm_filename, output_basename, start_address, extra_args):
         self.output_basename = output_basename
+        self.relocations = None
         pass # SFTODO
 
+    def add_relocations(self, other):
+        assert "ACORN_RELOCATABLE" in self.labels
+        assert self.asm_filename == other.asm_filename
+        assert self.start_address != other.start_address
+        assert set(self.extra_args) == set(other.extra_args)
+        self.relocations = SFTODO
 
+    def binary(self):
+        if "ACORN_RELOCATABLE" in self.labels:
+            # SFTODO: *Something* in this class needs to handle truncating at reloc_count
+            assert self.relocations is not None
+            return self.acme_output + self.relocations
+        else:
+            return self.acme_output
+
+
+# SFTODO: PATCH_VMEM IS A BIG SOURCE OF NOT-NECESSARILY-FATAL ERRORS, WHAT IS GOING TO CALL THAT AND HOW WILL I HANDLE THIS FAILING?
 def make_executable(asm_filename, start_address, extra_args):
     assert isinstance(start_address, int)
 
     filename_templates = {
-        "ozmoo": "ozmoo${ACORN_ELECTRON:_electron:}${ACORN_SWR:_swr:_tube}${ACORN_NO_SHADOW:_noshadow:}${VMEM::_novmem}${ACORN_SWR_SMALL_DYNMEM:_smalldyn:}_${STARTADDRESS}"
+        "ozmoo": "ozmoo${ACORN_ELECTRON:_electron:_bbc}${ACORN_SWR:_swr:_tube}${ACORN_NO_SHADOW:_noshadow:}${VMEM::_novmem}${ACORN_SWR_SMALL_DYNMEM:_smalldyn:}_${STARTADDRESS}"
     }
 
     basename = os.path.splitext(asm_filename)[0]
