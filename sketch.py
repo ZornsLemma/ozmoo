1a7603f34732179642318f6a1aab7f2a7a733acb
Steven Flintham <sgf@lemma.co.uk>
2020-09-22 14:58:03 +0100

    Snapshot

--- sketch.py
+++ sketch.py
@@ -1,11 +1,63 @@
+from __future__ import print_function
 import os
+import sys
 
-ozmoo_swr_args = ["-DVMEM=1", "-DACORN_SWR=1"]
-relocatable_args = ["-DACORN_RELOCATABLE=1"]
-small_dynmem_args = ["-DACORN_SWR_SMALL_DYNMEM=1"]
+def die(s):
+    print(s, file=sys.stderr)
+    sys.exit(1)
+
+def info(s):
+    if verbose_level >= 1:
+        print(s)
+
+def warn(s):
+    print("Warning: %s" % s, file=sys.stderr)
+
+def substitute(lst, a, b): # SFTODO: DELETE IF NOT USED
+    return [x.replace(a, b) for x in lst]
 
 def ourhex(i):
     return hex(i)[2:]
+
+def get_word(data, i):
+    return data[i]*256 + data[i+1]
+
+def divide_round_up(x, y):
+    if x % y == 0:
+        return x // y
+    else:
+        return (x // y) + 1
+
+def bytes_to_blocks(x):
+    return divide_round_up(x, 256)
+
+def run_and_check(args, output_filter=None):
+    if output_filter is None:
+        output_filter = lambda x: True
+    if verbose_level >= 2:
+        print(" ".join(args))
+    child = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    child.wait()
+    child_output = [line for line in child.stdout.readlines() if output_filter(line)]
+    # The child's stdout and stderr will both be output to our stdout, but that's not
+    # a big deal.
+    if (child.returncode != 0 or verbose_level >= 2) and len(child_output) > 0:
+        print("".join(x.decode(encoding="ascii") for x in child_output))
+    if child.returncode != 0:
+        die("%s failed" % args[0])
+
+def parse_labels(filename):
+    labels = {}
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            line = line[:-1]
+            components = line.split("=")
+            value = components[1].strip()
+            i = value.find(";")
+            if i != -1:
+                value = value[:i]
+            labels[components[0].strip()] = int(value.strip().replace("$", "0x"), 0)
+    return labels
 
 def template_substitute(template, args):
     d = {}
@@ -42,54 +94,81 @@ def template_substitute(template, args):
 # SFTODO: In a few places I am doing set(extra_args) - this is fine if all the elements stand alone like "-DFOO=1", but if there are multi-element entries ("--setpc", "$0900") I will need to do something different. I am not sure if this will be an issue or not.
 # SFTODO: I think an Executable object should have a min_swr property which would be 0 for tube builds or (maybe) small games which can run without SWR on a non-2P, 16 for most games and 32 for games which use all of first bank for dynmem. using K not bank count to ease things if I do ever support using e.g. private 12K on B+ where things aren't a multiple of 16.
 class Executable(object):
-    def __init__(self, asm_filename, output_basename, start_address, extra_args):
-        self.output_basename = output_basename
-        self.relocations = None
-        pass # SFTODO
+    # SFTODO: version is not a basename it is a version string
+    def __init__(self, asm_filename, version, start_address, extra_args):
+        self.asm_filename = asm_filename
+        self.version = version
+        self.start_address = start_address
+        self.extra_args = extra_args
+        self._relocations = None
+        basename = os.path.splitext(asm_filename)[0]
+        # SFTODO: Should really use the OS-local path join character in next few lines, not '/'
+        self._labels_filename = "temp/acme_labels_%s_%s" % (basename, version)
+        self._report_filename = "temp/acme_report_%s_%s" % (basename, version)
+        self._binary_filename = "temp/%s_%s" % (basename, version)
+        os.chdir("asm")
+        # SFTODO: Should really use the OS-local path join character in next few lines, not '/'
+        output_prefix = "../"
+        run_and_check(["acme", "--cpu", "6502", "--format", "plain", "--setpc", "$" + ourhex(start_address)] + self.extra_args + ["-l", output_prefix + self._labels_filename, "-r", output_prefix + self._report_filename, "--outfile", output_prefix + self._binary_filename])
+        os.chdir("..")
+        self._labels = parse_labels(self._labels_filename)
+
+        with open(self._binary_filename, "rb") as f:
+            self._binary = f.read()
+        if "ACORN_RELOCATABLE" in self._labels:
+            truncate_label = "reloc_count"
+        else:
+            truncate_label = "end_of_routines_in_stack_space"
+        self._binary = self._binary[:self._labels[truncate_label]-self._labels["program_start"]]
+
+        # SFTODO
+        #if "VMEM" in self._labels:
+        #    self._binary = Executable.patch_vmem(self._binary, self._labels)
 
     # SFTODO: Can/should we just automatically do the "other" build to make the relocations when we're asked for the binary?
     def add_relocations(self, other):
-        assert "ACORN_RELOCATABLE" in self.labels
+        assert "ACORN_RELOCATABLE" in self._labels
         assert self.asm_filename == other.asm_filename
         assert self.start_address != other.start_address
         assert set(self.extra_args) == set(other.extra_args)
-        self.relocations = SFTODO
+        return bytearray([]) # SFTODO! self._relocations = SFTODO
 
     def binary(self):
-        if "ACORN_RELOCATABLE" in self.labels:
-            # SFTODO: *Something* in this class needs to handle truncating at reloc_count
-            assert self.relocations is not None
-            return self.acme_output + self.relocations
+        if "ACORN_RELOCATABLE" in self._labels:
+            assert self._relocations is not None
+            return self._binary + self._relocations
         else:
-            return self.acme_output
+            return self._binary
 
 
 # SFTODO: PATCH_VMEM IS A BIG SOURCE OF NOT-NECESSARILY-FATAL ERRORS, WHAT IS GOING TO CALL THAT AND HOW WILL I HANDLE THIS FAILING? I THINK THIS IS THE ONLY LEGIT REASON FOR FAILING TO BUILD AN EXECUTABLE, THOUGH DO NOTE THAT IN SOME CASES (NOT SURE JUST NOW) IT MAY BE LEGIT FOR A BUILD DOING EXPERIMENTALLY TO FAIL ON THESE GROUPS, WE WOULD THEN JUST TWEAK PARAMS TO DO ANOTHER BUILD FOR THAT TARGET MACHINE
 def make_executable(asm_filename, start_address, extra_args):
     assert isinstance(start_address, int)
 
-    filename_templates = {
-        "ozmoo": "ozmoo${ACORN_ELECTRON:_electron:_bbc}${ACORN_SWR:_swr:_tube}${ACORN_NO_SHADOW:_noshadow:}${VMEM::_novmem}${ACORN_SWR_SMALL_DYNMEM:_smalldyn:}_${STARTADDRESS}"
+    version_templates = {
+        "ozmoo": "${ACORN_ELECTRON:_electron:_bbc}${ACORN_SWR:_swr:_tube}${ACORN_NO_SHADOW:_noshadow:}${VMEM::_novmem}${ACORN_SWR_SMALL_DYNMEM:_smalldyn:}_${STARTADDRESS}"
     }
 
     basename = os.path.splitext(asm_filename)[0]
-    assert basename in filename_templates
-    output_basename = template_substitute(filename_templates[basename], ["-DSTARTADDRESS=%s" % ourhex(start_address)] + extra_args)
+    assert basename in version_templates
+    version = template_substitute(version_templates[basename], ["-DSTARTADDRESS=%s" % ourhex(start_address)] + extra_args)
 
-    # Not all build parameters have to be reflected in the filename, but we
-    # can't have two builds with different parameters using the same filename.
+    # Not all build parameters have to be reflected in the version string, but
+    # we can't have two builds with different parameters using the same version
+    # string.
+    cache_key = (asm_filename, version)
     definition = (start_address, set(extra_args))
-    cache_entry = make_executable.cache.get(output_basename, None)
+    cache_entry = make_executable.cache.get(cache_key, None)
     if cache_entry is not None:
         assert cache_entry[0] == definition
         return cache_entry[1]
 
     # SFTODO: THIS NEEDS TO END UP RETURNING "NONE" IF IT'S A SMALLDYN BUILD AND GAME WON'T FIT IN MAIN RAM
     try:
-        e = Executable(asm_filename, output_basename, start_address, extra_args)
+        e = Executable(asm_filename, version, start_address, extra_args)
     except GameWontFit:
         e = None
-    make_executable.cache[output_basename] = (definition, e)
+    make_executable.cache[cache_key] = (definition, e)
     return e
 make_executable.cache = {}
 
@@ -206,15 +285,31 @@ def make_electron_swr_executable():
     return make_optimally_aligned_executable(0x1d00, extra_args)
     
 
-    
+header_version = 0
+header_static_mem = 0xe
+vmem_block_pagecount = 2
+min_timestamp = 0
+max_timestamp = 0xe0 # initial tick value
+verbose_level = 2 # SFTODO TEMP HACK SHOULD BE PARSED FROM ARGS
 
-e = make_executable("ozmoo.asm", 0x900, [])
-print e.output_basename
-e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1"])
-print e.output_basename
-e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_NO_SHADOW=1", "-DVMEM=1"])
-print e.output_basename
-#e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_ELECTRON=1", "-DACORN_SWR_SMALL_DYNMEM=1", "-DVMEM=1"])
-#print e.output_basename
-e = make_executable("ozmoo.asm", 0x900, ["-DACORN_SWR=1", "-DACORN_ELECTRON=1", "-DACORN_SWR_SMALL_DYNMEM=1", "-DVMEM=1", "-DFOO=1"])
-print e.output_basename
+ozmoo_swr_args = ["-DVMEM=1", "-DACORN_SWR=1"]
+relocatable_args = ["-DACORN_RELOCATABLE=1"]
+small_dynmem_args = ["-DACORN_SWR_SMALL_DYNMEM=1"]
+
+with open(sys.argv[1], "rb") as f : # SFTODO with open(args.input_file, "rb") as f:
+    game_data = bytearray(f.read())
+game_blocks = bytes_to_blocks(len(game_data))
+dynamic_size_bytes = get_word(game_data, header_static_mem)
+nonstored_blocks = bytes_to_blocks(dynamic_size_bytes)
+while nonstored_blocks % vmem_block_pagecount != 0:
+    nonstored_blocks += 1
+
+ozmoo_base_args = [ # SFTODO: MOVE THIS?
+    "-DACORN=1",
+    "-DACORN_CURSOR_PASS_THROUGH=1",
+    "-DSTACK_PAGES=4",
+    "-DSMALLBLOCK=1",
+    "-DSPLASHWAIT=0",
+    "-DACORN_INITIAL_NONSTORED_BLOCKS=%d" % nonstored_blocks,
+    "-DACORN_DYNAMIC_SIZE_BYTES=%d" % dynamic_size_bytes,
+]
