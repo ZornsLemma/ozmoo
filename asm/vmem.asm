516e53fff5d1af9773e493c195767406657bea18
Steven Flintham <sgf@lemma.co.uk>
2021-01-23 16:23:35 +0000

    Small improvement

--- asm/vmem.asm
+++ asm/vmem.asm
@@ -47,6 +47,7 @@ inc_vmem_cache_cnt
 }
 }
 
+; SFTODO: EXPERIMENTAL mempointer+Y OPTIMISATIONS NOT DONE ON THIS NON-VMEM BRANCH YET
 !ifndef VMEM {
 ; Non-virtual memory
 
@@ -676,8 +677,7 @@ read_byte_at_z_address
 }
 
 
-; SFTODO: COULD WE DEFER THIS STY MEMPOINTER UNTIL .non_dynmem BELOW, AND THEN NOT DO LDY #0 AT READ_AND_RETURN_VALUE? THAT WOULD SAVE US 5 CYCLES PER CALL IN THE DYNMEM CASE FOR ESSENTIALLY NO PENALTY IN THE NONDYNMEM CASE. AH, NO, WE DON'T *KNOW* mempointer (LOW BYTE) IS 0, AND IN GENERAL I DON'T THINK WE CAN NECESSARILY MAINTAIN IT LIKE THAT. IT *MIGHT* BE POSSIBLE THOUGH, HAVE A GOOD LOOK AT THE CODE/THINK BEFORE DISMISSING THIS POSSIBILITY.
-	sty mempointer_y ; low byte unchanged
+	; sty mempointer_y ; low byte unchanged
 	; same page as before?
 	cpx zp_pc_l
 	bne .read_new_byte
@@ -689,7 +689,7 @@ read_byte_at_z_address
     +acorn_page_in_bank_using_a mempointer_ram_bank
 }
 .read_and_return_value
-	ldy mempointer_y ; ldy #0
+	; ldy mempointer_y ; ldy #0
 	+before_dynmem_read
     ; SFTODO: THIS WOULD PROBABLY BENEFIT FROM CMOS (ZP) ADDRESSING MODE, IT IS HOT
 	lda (mempointer),y
@@ -740,6 +740,7 @@ read_byte_at_z_address
 }
 }
 .non_dynmem
+	sty mempointer_y ; low byte unchanged
 	lsr
 	sta vmem_temp + 1
 	lda #0
