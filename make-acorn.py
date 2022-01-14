d037d49e1472a19a9713e7310d6f8eca9b23ed69
Steven Flintham <sgf@lemma.co.uk>
2022-01-14 22:44:30 +0000

    strip() values put into tool_versions
    The line terminator causes (admittedly minor) inconsistencies in build
    files on Linux and Windows.

--- make-acorn.py
+++ make-acorn.py
@@ -183,7 +183,7 @@ def get_tool_version(name):
             version_components = c[1].split(".")
             if len(version_components) >= 2:
                 version = tuple(int("0" + re.findall("^\d+", x)[0]) for x in version_components[:2])
-                string_version = line
+                string_version = line.strip()
                 break
     global tool_versions
     if name not in string_version:
