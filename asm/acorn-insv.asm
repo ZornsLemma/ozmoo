024d9193e4fbbf3283993045fcb7e1f0c17a4ea5
Steven Flintham <sgf@lemma.co.uk>
2022-01-22 02:19:19 +0000

    WIP snapshot of INSV handler tweaks

--- asm/acorn-insv.asm
+++ asm/acorn-insv.asm
@@ -9,7 +9,20 @@
 insv = $22a
 vdu_status = $d0
 cursor_key_status = $27d ; address updated by *FX4
+copy_key = 139
 
+; SFTODO: We could claw a byte back by moving nominal_cursor_key_status to a fixed location in page 4/5/6,
+; although it's perhaps going to be fiddly to do so (we don't want to "waste" a byte for it on tube client side).
+; We could also claw another byte back by moving nominal_cursor_key_status into zp, but that's probably a bit
+; wasteful of a valuable byte of ZP.
+; SFTODO: Hmm, we *could* change "lda nominal_cursor_key_status" - our *only* reference to this - to "lda #x" and
+; make the immediate operand address nominal_cursor_key_status. That would save two bytes and is localised hackery
+; rather than interfering with zp/low memory allocation which is more global.
+; SFTODONOW: *Probably* nothing will do a VDU 7 when Ozmoo is running, but it might be possible - e.g. a * command
+; might do it. Should we arrange (I think there's a *FX call) for VDU 7 beep to use sound channel 3 to avoid
+; trampling on this code? (Might be good to confirm just at BASIC prompt this *can* be a problem first.)
+; SFTODO: Not super keen, but we could maybe squeeze this code into CFS/RFS workspace &380-&3DF inclusive instead
+; of the sound buffers.
 !if * != nominal_cursor_key_status {
     !error "nominal_cursor_key_status incorrect"
 }
@@ -18,25 +31,39 @@ cursor_key_status = $27d ; address updated by *FX4
 ; On entry:
 ;     A=140-143 for unshifted cursors
 ;     A=156-159 for shifted cursors
+;     X=buffer number
+;
+; On exit:
+;     A, X preserved
+;     Y undefined
+;     C indicates insertion success/failure, but parent INSV takes care of that
 our_insv
-    cpx #buffer_keyboard
+    tay ; save A for later reference
+    +assert buffer_keyboard = 0
+    txa ; saves a byte compared to cpx #buffer_keyboard
     bne jmp_old_insv
     ; If split cursor editing is in progress, leave things alone.
     bit vdu_status
     bvs jmp_old_insv
-    tax
     ; If nominal_cursor_key_status is 1, we always use that.
     lda nominal_cursor_key_status
     bne cursor_key_status_in_a
+    ; SFTODO: COMMENT
+    cpy #copy_key
+    bne SFTODOA
+    ;ldy #159
+    ;jsr SFTODOX
+    ;ldy #158
+SFTODOA
     ; Check if this is a shifted or unshifted cursor key.
-    txa
+    tya
     and #%11101100
     cmp #%10001100
     bne not_unshifted_or_shifted_cursor
     ; This is a shifted (b4 set) or unshifted (b4 clear) cursor key. Set
     ; cursor_key_status = !b4, so unshifted cursor keys return codes to the
     ; application and shifted cursor keys trigger split cursor editing.
-    txa
+    tya
     and #%00010000
     eor #%00010000
     beq cursor_key_status_in_a
@@ -44,8 +71,8 @@ our_insv
 cursor_key_status_in_a
     sta cursor_key_status
 not_unshifted_or_shifted_cursor
-    txa
-    ldx #buffer_keyboard
+SFTODOX
+    tya
 jmp_old_insv
     jmp $ffff ; patched by init
 
