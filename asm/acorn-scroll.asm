460ef2fead1345c5fba42b75da45df77547195e9
Steven Flintham <sgf@lemma.co.uk>
2023-05-06 23:35:22 +0100

    WIP scrolling experiment

--- asm/acorn-scroll.asm
+++ asm/acorn-scroll.asm
@@ -7,6 +7,7 @@ vdu_text_window_bottom = $309
 vdu_text_cursor_y_position = $319
 vdu_screen_top_left_address_low = $350
 vdu_screen_top_left_address_high = $351
+vdu_screen_size_high_byte = $354
 set_cursor_software_and_hardware_position = $c6af
 hardware_scroll_up = $c9a4
 move_text_cursor_to_next_line = $cd3f ; SFTODO: probably does more than strictly needed FWIW
@@ -50,36 +51,55 @@ lf
     pha
     tya
     pha
-    lda vdu_screen_top_left_address_high
-    sta src+1
     lda vdu_screen_top_left_address_low
     sta src
-    clc
-    adc #<bytes_per_line
-    sta dst
     lda vdu_screen_top_left_address_high
-    adc #>bytes_per_line
-    sta dst+1
+    sta src+1
     jsr move_text_cursor_to_next_line
     jsr hardware_scroll_up
-    ldx #>bytes_per_line
-    ldy #255
+    lda vdu_screen_top_left_address_low
+    sta dst
+    lda vdu_screen_top_left_address_high
+    sta dst+1
+    ; We copy and bump in chunks of 128 because the line length in bytes is
+    ; always a multiple of 128, which means we can wrap at the top of screen
+    ; memory between each chunk without any problems.
+    ldx #(bytes_per_line / 128)
+copy_and_zero_outer_loop
+    ldy #127
 copy_and_zero_loop
     lda (src),y
     sta (dst),y
     lda #%10101010
     sta (src),y
     dey
-    cpy #255
-    bne copy_and_zero_loop
+    bpl copy_and_zero_loop
+    clc
+    lda src
+    adc #128
+    sta src
+    bcc no_src_wrap
     inc src+1
+    bpl no_src_wrap
+    sec
+    lda src+1
+    sbc vdu_screen_size_high_byte
+    sta src+1
+no_src_wrap
+    clc
+    lda dst
+    adc #128
+    sta dst
+    bcc no_dst_wrap
     inc dst+1
+    bpl no_dst_wrap
+    sec
+    lda dst+1
+    sbc vdu_screen_size_high_byte
+    sta dst+1
+no_dst_wrap
     dex
-    bmi copy_and_zero_done
-    bne copy_and_zero_loop
-    ldy #(<bytes_per_line)-1
-    bne copy_and_zero_loop ; always branch
-copy_and_zero_done
+    bne copy_and_zero_outer_loop
     jsr set_cursor_software_and_hardware_position
     pla
     tay
