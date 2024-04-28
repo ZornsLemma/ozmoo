ee8979b818693a45233730d48c87697170aa4c55
Steven Flintham <sgf@lemma.co.uk>
2024-04-28 14:28:38 +0100

    Fix Python 3 only code in previous commit

--- make-acorn.py
+++ make-acorn.py
@@ -2314,7 +2314,7 @@ zp_bytes_to_save = 0xd
 if not os.path.exists("temp"):
     os.makedirs("temp")
 # Create a cache directory tag in temp. See https://bford.info/cachedir/
-with open(os.path.join("temp", "CACHEDIR.TAG"), "w", encoding="ascii") as f:
+with open(os.path.join("temp", "CACHEDIR.TAG"), "w") as f:
     f.write("Signature: 8a477f597d28d172789f06886806bc55")
 
 host = 0xffff0000
