ZornsLemma/ozmoo

commit 2a6960cc203cd92893831ef0949b8e5b6b4fc16f

Re-use fast scroll code allocation for slow scroll buffer
Lightly tested and seems to work. (I used a B+ in mode 3, and to force
no fast scrolling I hackily replaced the FASTSCR executable with a stub
that just set the flag to 0 and returned.)

asm/acorn-shared-constants.asm
@@ -205,11 +205,26 @@ xxx_fast_scroll_private_ram_end = $b000
     insv_resident_end = 0x932
 }
 
+!ifdef ACORN_HW_SCROLL_SLOW {
+    ACORN_HW_SCROLL_FAST_OR_SLOW = 1
+} else {
+    !ifdef ACORN_HW_SCROLL_FAST {
+        ACORN_HW_SCROLL_FAST_OR_SLOW = 1
+    }
+}
+
+max_screen_width = 80
+
+!ifdef ACORN_HW_SCROLL_FAST_OR_SLOW {
+    xxx_fast_scroll_start = 0x932
+    xxx_fast_scroll_end = 0xb00
+}
+
 !ifdef ACORN_HW_SCROLL_FAST {
     fast_scroll_upper_window_size = xxx_shadow_driver_end + 1 + xxx_max_ram_bank_count + 1 ; 1 byte
 
-    fast_scroll_start = 0x932
-    fast_scroll_end = 0xb00
+    fast_scroll_start = xxx_fast_scroll_start
+    fast_scroll_end = xxx_fast_scroll_end
 
     fast_scroll_max_upper_window_size = 3
 
@@ -219,6 +234,16 @@ xxx_fast_scroll_private_ram_end = $b000
     ; access screen RAM directly.
     fast_scroll_private_ram = xxx_fast_scroll_private_ram
     fast_scroll_private_ram_end = xxx_fast_scroll_private_ram_end
+}
+
+!ifdef ACORN_HW_SCROLL_SLOW {
+    ; Although the same Ozmoo executable can support fast and slow hardware
+    ; scrolling, at runtime we are always using one or the other, so we can use
+    ; the space allocated for the fast hardware scrolling machine code for the
+    ; slow hardware scrolling line buffer. SFTODONOW: DOUBLE CHECK THIS IS VALID
+    top_line_buffer = xxx_fast_scroll_start
+    top_line_buffer_reverse = top_line_buffer + max_screen_width
+    +assert top_line_buffer_reverse + max_screen_width <= xxx_fast_scroll_end
 }
 
 +assert xxx_shadow_driver_end + 1 + xxx_max_ram_bank_count + 1 < $900

asm/acorn-ozmoo-constants.asm
@@ -98,7 +98,6 @@ mode_7_text_colour_base = 128
 mode_7_graphics_colour_base = 144
 err_not_found = $d6
 ctrl_key_adjust = 64
-max_screen_width = 80
 !ifdef ACORN_SWR {
 flat_ramtop = $8000
 swr_ramtop = $c000
@@ -877,22 +876,6 @@ jmp_buf	+allocate jmp_buf_size
 	+pre_allocate 60
 streams_stack
 	+allocate 60
-
-!ifdef ACORN_HW_SCROLL_SLOW {
-    ; SFTODONOW: This does take up a lot of space in main RAM. If we could find
-    ; somewhere else to shove it, it would probably go a long way towards
-    ; lowering data_start. Hmm, can I reuse the space in pages 9/&A allocated to
-    ; the fast scroll code? Given you can't toggle between slow and fast HW
-    ; scrolling at runtime, I think if (at runtime; the ACORN_HW_SCROLL_SLOW
-    ; build-time flag just says we support it if necessary) we are using slow HW
-    ; scrolling, we know the fast scroll code space is free.
-	+pre_allocate max_screen_width
-top_line_buffer
-	+allocate max_screen_width
-	+pre_allocate max_screen_width
-top_line_buffer_reverse
-	+allocate max_screen_width
-}
 ; }}}
 
 ; {{{ Final allocations and checks depending on earlier allocations

asm/ozmoo.asm
@@ -360,16 +360,6 @@ ACORN_SWR_MEDIUM_OR_BIG_DYNMEM = 1
 }
 }
 
-; Similarly, we create a constant for having support for either kind of hardware
-; scrolling.
-!ifdef ACORN_HW_SCROLL_SLOW {
-ACORN_HW_SCROLL_FAST_OR_SLOW = 1
-} else {
-!ifdef ACORN_HW_SCROLL_FAST {
-ACORN_HW_SCROLL_FAST_OR_SLOW = 1
-}
-}
-
 ; The screen hole mostly "just works" for the small dynamic memory model;
 ; because the hole always comes *after* all dynamic memory, only read-only VM
 ; block access needs to take it into account. Define an extra constant to allow

commit 5b3b82d275c28ec5e8e71066ed026843f40e70fb

Fix broken slow hardware scrolling during initial output
Commit 2a6960cc203cd92893831ef0949b8e5b6b4fc16f broke this as I didn't
realise I was relying on the zero-initialisation of top_line_buffer. See
the comments added in this commit for more on that.

I noticed this while testing a change for removing redundant soft
hardware scrolling code on B-no-shadow builds. I saw a hang with a no
fast hardware scrolling build, which I think was caused by an accidental
VDU 2 and the printer buffer filling up. Without getting unlucky and
hitting a VDU 2, this bug would probably just cause some initial screen
corruption, which I have also seen during testing.

I am not quite sure why my quick test of commit 2a6960c didn't detect
this; I can only assume I somehow got lucky and the data in the buffer
didn't contain any VDU 2 operations and maybe I didn't notice the brief
corruption. I'd have thought the fact we run FINDSWR/INSV first and thus
"initialise" this memory with some relatively fixed data would have
meant this went wrong consistently, but I'm not digging into it now I've
found it. (My hacky FASTSCR would probably have overwritten a 2 byte
near &932 left there by INSV, but it's not clear to me that's a full
explanation.)

asm/acorn-init-preload.asm
@@ -83,6 +83,30 @@ deletable_init_start
     cpx #low_fixed_gap_start - low_start
     bne -
 
+    ; Make sure the top_line_buffer* buffers are zero-initialised; this won't
+    ; happen automatically because they re-use the memory allocated to the fast
+    ; hardware scrolling code. (That memory will contain the fast scrolling code
+    ; if this build supports it, or whatever is left in that memory otherwise.)
+    ; We must make sure we don't trample over that code if it is actually going
+    ; to be used.
+    ;
+    ; The only reason this is necessary is because for Z1-3 games erase_window
+    ; doesn't clear this line and so until the game actually outputs on this
+    ; line slow hardware scrolling will use the initial contents of the buffer.
+!ifdef ACORN_HW_SCROLL_SLOW {
+!ifdef ACORN_HW_SCROLL_FAST {
+    lda fast_scroll_status
+    bne +
+}
+    lda #0
+    tax
+-   sta top_line_buffer,x
+    inx
+    cpx #top_line_buffer_reverse_end - top_line_buffer
+    bne -
++
+}
+
     ; Clear memory betwen bulk_clear_start and bulk_clear_end; some of the
     ; variables which may live here need to be zero initialised and it's good
     ; for consistency to clear everything.

asm/acorn-init-stack.asm
@@ -492,6 +492,10 @@ deletable_screen_init_2
 	ldy #1
 	sty is_buffered_window
 	ldx #$ff
+    ; SF: Note that for Z1-3 this clears from line 1 down, not line 0. This is not a big deal and
+    ; I am not going to try to change it, but this is why we need to zero-initialise top_line_buffer
+    ; for when we are using slow hardware scrolling, as the buffer will not be set to all spaces
+    ; by this erase operation.
 	jsr erase_window
 	jmp start_buffering
 ; End of deletable_screen_init_2

asm/acorn-shared-constants.asm
@@ -243,7 +243,8 @@ max_screen_width = 80
     ; slow hardware scrolling line buffer. SFTODONOW: DOUBLE CHECK THIS IS VALID
     top_line_buffer = xxx_fast_scroll_start
     top_line_buffer_reverse = top_line_buffer + max_screen_width
-    +assert top_line_buffer_reverse + max_screen_width <= xxx_fast_scroll_end
+    top_line_buffer_reverse_end = top_line_buffer_reverse + max_screen_width
+    +assert top_line_buffer_reverse_end <= xxx_fast_scroll_end
 }
 
 +assert xxx_shadow_driver_end + 1 + xxx_max_ram_bank_count + 1 < $900